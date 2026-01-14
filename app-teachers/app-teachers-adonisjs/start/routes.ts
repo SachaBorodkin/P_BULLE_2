c4e347609359ccdc1b99d2496ad3647b97b31cba
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2026-01-14 16:29:34 +0100

    Update routes.ts
    [30][WiP]

--- app-teachers/app-teachers-adonisjs/start/routes.ts
+++ app-teachers/app-teachers-adonisjs/start/routes.ts
@@ -9,3 +9,4 @@
 import TeachersController from '#controllers/teachers_controller'
 import router from '@adonisjs/core/services/router'
 router.get('/', [TeachersController, 'index']).as('home')
+router.get('/teacher/:id/show', [TeachersController, 'show']).as('teacher.show')
