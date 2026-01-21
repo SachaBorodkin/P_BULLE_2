3aefc7c740846863ec5d076c049a4a693ba523bb
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2026-01-21 16:09:10 +0100

    Step 13
    [50][DONE]

--- app-teachers/app-teachers-adonisjs/start/routes.ts
+++ app-teachers/app-teachers-adonisjs/start/routes.ts
@@ -6,17 +6,34 @@
 | Le fichier des routes a pour but de définir toutes les routes HTTP.
 |
 */
-import TeachersController from '#controllers/teachers_controller'
 import AuthController from '#controllers/auth_controller'
+import TeachersController from '#controllers/teachers_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+// La route de la homepage est accessible aussi bien à un utilisateur authentifié que non authentifié
 router.get('/', [TeachersController, 'index']).as('home')
-router.get('/teacher/:id/show', [TeachersController, 'show']).as('teacher.show')
-router.delete('/teacher/:id/destroy', [TeachersController, 'destroy']).as('teacher.destroy')
-router.get('/teacher/add', [TeachersController, 'create']).as('teacher.create')
-// Route permettant l'ajout de l'enseignant
-router.post('/teacher/add', [TeachersController, 'store']).as('teacher.store')
-router.get('/teacher/:id/edit', [TeachersController, 'edit']).as('teacher.edit')
-// Route permettant la modification de l'enseignant
-router.put('/teacher/:id/update', [TeachersController, 'update']).as('teacher.update')
-router.post('/login', [AuthController, 'login']).as('auth.login')
-router.post('/logout', [AuthController, 'logout']).as('auth.logout')
+// Route permettant de voir les détails d'un enseignant
+router
+  .get('/teacher/:id/show', [TeachersController, 'show'])
+  .as('teacher.show')
+  .use(middleware.auth())
+// Toutes les routes de ce groupe sont soumises à l'authentification
+router
+  .group(() => {
+    // Route permettant d'afficher le formulaire permettant l'ajout d'un enseignant
+    router.get('/teacher/add', [TeachersController, 'create']).as('teacher.create')
+    // Route permettant l'ajout de l'enseignant
+    router.post('/teacher/add', [TeachersController, 'store']).as('teacher.store')
+    // Route permettant d'afficher le formulaire permettant la mise à jour d'un enseignant
+    router.get('/teacher/:id/edit', [TeachersController, 'edit']).as('teacher.edit')
+    // Route permettant la modification de l'enseignant
+    router.put('/teacher/:id/update', [TeachersController, 'update']).as('teacher.update')
+    // Route permettant de supprimer un enseignant
+    router.delete('/teacher/:id/destroy', [TeachersController, 'destroy']).as('teacher.destroy')
+  })
+  .use(middleware.auth())
+  .use(middleware.ensureAdmin())
+// Route permettant de se connecter accessible uniquement à un utilisateur non authentifié
+router.post('/login', [AuthController, 'login']).as('auth.login').use(middleware.guest())
+// Route permettant de se déconnecter accessible uniquement à un utilisateur authentifié
+router.post('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
