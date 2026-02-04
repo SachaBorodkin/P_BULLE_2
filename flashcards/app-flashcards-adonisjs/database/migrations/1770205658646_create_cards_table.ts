ef7cb7656bfe78e5d8dfffed7a1c8a1b5e27f588
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2026-02-04 12:53:52 +0100

    migrations
    [10][DONE]

--- flashcards/app-flashcards-adonisjs/database/migrations/1770205658646_create_cards_table.ts
+++ flashcards/app-flashcards-adonisjs/database/migrations/1770205658646_create_cards_table.ts
@@ -1,24 +1,16 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'decks'
+  protected tableName = 'cards'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name').notNullable()
-      table.string('description').nullable()
-
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-
+      table.string('question').notNullable()
+      table.string('answer').notNullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
+      table.integer('deck_id').unsigned().references('id').inTable('decks').onDelete('CASCADE')
     })
   }
 
